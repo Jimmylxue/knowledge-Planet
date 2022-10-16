fde440c754f48d9b54d1f5d2f5ce2d2829775814
jimmylxuexue <1002661758@qq.com>
2022-10-16 22:45:55 +0800

    :sparkles: add githubContribution

---
+++ src/pages/Home/const.ts
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    id: 0,
+    name: '诺基亚图片生成器',
+    url: 'http://www.jimmyxuexue.top:667/Nokia/',
+  },
+  {
+    id: 1,
+    name: '元气提醒',
+    url: 'https://github.com/Jimmylxue/Vitality-reminder',
+  },
+  {
+    id: 2,
+    name: '水印（马赛克）生成器',
+    url: 'https://github.com/Jimmylxue/easy-watermark',
+  },
+  {
+    id: 3,
+    name: '雪人消消乐',
+    url: 'https://github.com/Jimmylxue/clear-by-egret',
+  },
+  {
+    id: 4,
+    name: '笨鸟游戏',
+    url: 'https://github.com/Jimmylxue/bird-by-egret',
+  },
+  {
+    id: 5,
+    name: '飞机大战',
+    url: 'https://github.com/Jimmylxue/aircraft-battle-by-egret',
+  },
+  {
+    id: 6,
+    name: '50个前端项目',
+    url: 'https://github.com/Jimmylxue/project-small-cases',
+  },
+];
+
+export const otherSite = [
+  {
+    id: 1,
+    name: '前端加油站',
+    url: 'http://www.jimmyxuexue.top:999/',
+  },
+  {
+    id: 2,
+    name: 'bilibili',
+    url: 'https://space.bilibili.com/304985153?spm_id_from=333.1007.0.0',
+  },
+  {
+    id: 3,
+    name: '时间管理大师（学习时钟）',
+    url: 'http://www.jimmyxuexue.top:667/study/',
+  },
+  {
+    id: 4,
+    name: '做图神器',
+    url: 'https://www.logoly.pro/#/',
+  },
+];
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,67 +1,8 @@
 import { Button, message } from 'antd';
 import { LinkOutlined } from '@ant-design/icons';
+import { GithubContributions } from 'github-contributions-react';
+import { projects, otherSite } from './const';
 import './index.less';
-
-const projects = [
-  {
-    id: 0,
-    name: '诺基亚图片生成器',
-    url: 'http://www.jimmyxuexue.top:667/Nokia/',
-  },
-  {
-    id: 1,
-    name: '元气提醒',
-    url: 'https://github.com/Jimmylxue/Vitality-reminder',
-  },
-  {
-    id: 2,
-    name: '水印（马赛克）生成器',
-    url: 'https://github.com/Jimmylxue/easy-watermark',
-  },
-  {
-    id: 3,
-    name: '雪人消消乐',
-    url: 'https://github.com/Jimmylxue/clear-by-egret',
-  },
-  {
-    id: 4,
-    name: '笨鸟游戏',
-    url: 'https://github.com/Jimmylxue/bird-by-egret',
-  },
-  {
-    id: 5,
-    name: '飞机大战',
-    url: 'https://github.com/Jimmylxue/aircraft-battle-by-egret',
-  },
-  {
-    id: 6,
-    name: '50个前端项目',
-    url: 'https://github.com/Jimmylxue/project-small-cases',
-  },
-];
-
-const otherSite = [
-  {
-    id: 1,
-    name: '前端加油站',
-    url: 'http://www.jimmyxuexue.top:999/',
-  },
-  {
-    id: 2,
-    name: 'bilibili',
-    url: 'https://space.bilibili.com/304985153?spm_id_from=333.1007.0.0',
-  },
-  {
-    id: 3,
-    name: '时间管理大师（学习时钟）',
-    url: 'http://www.jimmyxuexue.top:667/study/',
-  },
-  {
-    id: 4,
-    name: '做图神器',
-    url: 'https://www.logoly.pro/#/',
-  },
-];
 
 const HomePage = () => {
   return (
@@ -157,6 +98,9 @@ const HomePage = () => {
             </Button>
           ))}
         </div>
+      </div>
+      <div className="dz-git-show">
+        <GithubContributions username="Jimmylxue" color="rgb(66, 184, 131)" />
       </div>
     </div>
   );
